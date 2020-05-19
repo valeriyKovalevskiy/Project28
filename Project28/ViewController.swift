c760b3bcff4a6865f576276b2eeb3cd401ebb351
Valeriy Kovalevskiy <v.kovalevskiy.ios@gmail.com>
2020-05-19 17:20:47 +0300

    Project 28 completed

--- Project28/ViewController.swift
+++ Project28/ViewController.swift
@@ -7,14 +7,80 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var secret: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        title = "nothing to see here"
+        
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(saveSecretMessage), name: UIApplication.willResignActiveNotification, object: nil)
+    
     }
 
-
+    @IBAction func authenticateTapped(_ sender: UIButton) {
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "Identify yourself"
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { [weak self] (success, error) in
+                DispatchQueue.main.async {
+                    if success {
+                        self?.unlockSecretMessage()
+                    } else {
+                        //error
+                        let ac = UIAlertController(title: "Auth failed", message: "Try again", preferredStyle: .alert)
+                        ac.addAction(UIAlertAction(title: "OK", style: .default))
+                        self?.present(ac, animated: true)
+                    }
+                }
+            }
+        } else {
+            //no biometry
+            let ac = UIAlertController(title: "Auth failed", message: "Device not configured for biometric auth", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        }
+    }
+    
+    @objc func adjustForKeyboard(notification: Notification) {
+        guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+        let keyboardScreenEnd = keyboardValue.cgRectValue
+        let keyboardViewEndFrame = view.convert(keyboardScreenEnd, from: view.window)
+        
+        if notification.name == UIResponder.keyboardWillHideNotification {
+            secret.contentInset = .zero
+        } else {
+            secret.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height - view.safeAreaInsets.bottom, right: 0)
+        }
+        
+        secret.scrollIndicatorInsets = secret.contentInset
+        
+        let selectedRange = secret.selectedRange
+        secret.scrollRangeToVisible(selectedRange)
+    }
+    
+    func unlockSecretMessage() {
+        secret.isHidden = false
+        title = "secret stuff here"
+        
+        secret.text = KeychainWrapper.standard.string(forKey: "SecretMessage") ?? ""
+    }
+    
+    @objc func saveSecretMessage() {
+        guard secret.isHidden == false else { return }
+        
+        KeychainWrapper.standard.set(secret.text, forKey: "SecretMessage")
+        secret.resignFirstResponder()
+        secret.isHidden = true
+        title = "nothing to see here"
+    }
 }
 
