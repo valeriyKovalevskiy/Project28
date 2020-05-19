e74fe6ee7da328f87c93a881663e320631495323
Valeriy Kovalevskiy <v.kovalevskiy.ios@gmail.com>
2020-05-19 15:41:50 +0300

    Initial Commit

---
+++ Project28/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project28
+//
+//  Created by Valeriy Kovalevskiy on 5/19/20.
+//  Copyright © 2020 v.kovalevskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
